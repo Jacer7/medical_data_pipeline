0bd3423a1889e8ad09879b7e6e83bcc16b7a6123
jacer7 <jsor.khadka@gmail.com>
2025-09-16 23:46:53 +0200

    corrected docker image for dagster

--- sql/init/create_databases.sql
+++ sql/init/create_databases.sql
@@ -1,5 +1,6 @@
--- Create the medical_data database
+CREATE DATABASE dagster_backend;
 CREATE DATABASE medical_data;
 
 -- Grant permissions
 GRANT ALL PRIVILEGES ON DATABASE medical_data TO pipeline_user;
+GRANT ALL PRIVILEGES ON DATABASE dagster_backend TO pipeline_user;
